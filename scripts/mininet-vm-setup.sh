21477a7d96b7293c82e631696d7fb5165d49f0de
Faizaan Chishtie <faizchishtie@gmail.com>
2021-01-23 14:44:06 -0500

    update script

--- scripts/mininet-vm-setup.sh
+++ scripts/mininet-vm-setup.sh
@@ -2,9 +2,23 @@ sudo apt update
 sudo apt upgrade
 
 sudo apt install git
+sudo apt install curl
+sudo apt install gcc openssl-devel bzip2-devel libffi-devel
 
-sudo apt install python3.6
-sudo apt install python3-pip
+cd /opt
+wget https://www.python.org/ftp/python/3.7.9/Python-3.7.9.tgz
+sudo tar xzf Python-3.7.9.tgz
+
+cd Python-3.7.9
+sudo ./configure --enable-optimizations
+sudo make altinstall
+python3 --version
+
+alias python='/usr/bin/python3.7.9'
+alias python3='/usr/bin/python3.7.9'
+
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python get-pip.py
 
 echo 'if [ -d "$HOME/bin" ] ; then\n\tPATH="$PATH:$HOME/bin"\nfi' > ~/.bash_profile
 
