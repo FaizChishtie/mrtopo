6e1004e2d9030e859bc8fb45c0cd676d03197027
Faizaan Chishtie <faizchishtie@gmail.com>
2021-01-23 15:18:14 -0500

    update sh

--- scripts/mininet-vm-setup.sh
+++ scripts/mininet-vm-setup.sh
@@ -1,11 +1,18 @@
 sudo apt update
-sudo apt upgrade
+yes | sudo apt upgrade
+sudo apt-get update
+yes | sudo apt-get upgrade
 
-sudo apt install git
-sudo apt install curl
-sudo apt install gcc openssl-devel bzip2-devel libffi-devel
+yes | sudo apt install git
+yes | sudo apt install curl
+yes | sudo apt install gcc openssl-devel bzip2-devel libffi-devel
 
-cd /opt
+yes | sudo apt-get install wget build-essential checkinstall
+yes | sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev \
+    libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev
+
+
+cd /usr/src
 wget https://www.python.org/ftp/python/3.7.9/Python-3.7.9.tgz
 sudo tar xzf Python-3.7.9.tgz
 
@@ -14,8 +21,8 @@ sudo ./configure --enable-optimizations
 sudo make altinstall
 python3 --version
 
-alias python='/usr/bin/python3.7.9'
-alias python3='/usr/bin/python3.7.9'
+alias python=python3.7
+alias python3=python3.7
 
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python get-pip.py
@@ -27,6 +34,8 @@ source ~/.bash_profile
 pip3 install mrtopo
 
 ## ONOS INSTALL
+
+cd /home/mininet/mrtopo/scripts
 
 sh install-onos-dependencies.sh
 sh onos-install.sh
