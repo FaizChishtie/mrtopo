901d778860dd2d34c2280e413a8320d3de530713
Faizaan Chishtie <faizchishtie@gmail.com>
2021-01-24 16:36:09 -0500

    add script

--- scripts/mrtopo-scripts.sh
+++ scripts/mrtopo-scripts.sh
@@ -12,41 +12,6 @@ prompt() {
   echo "Type 'q' to quit"
 }
 
-select_script () {
-  if [ "$1" = "q" ]
-  then
-    echo "Bye."
-    exit 1
-  fi
-  echo "Selected option $option"
-  if [ "$1" = "0" ]
-  then
-    echo "Full VM Install"
-    return 1
-  fi
-  if [ "$1" = "1" ]
-  then
-    echo "Python 3.7.9 Install"
-    return 1
-  fi
-  if [ "$1" = "2" ]
-  then
-    echo "Onos Install"
-    return 1
-  fi
-  if [ "$1" = "3" ]
-  then
-    echo "Create Virtual Environment"
-    return 1
-  fi
-  if [ "$1" = "4" ]
-  then
-    echo "Remove Virtual Environment"
-    return 1
-  fi
-  return 0
-}
-
 full_vm() {
   sudo sh setup/mininet-vm-setup.sh
 }
@@ -68,6 +33,47 @@ remove_test_env() {
   sudo sh test-env/rm-test-env.sh
 }
 
+select_script () {
+  if [ "$1" = "q" ]
+  then
+    echo "Bye."
+    exit 1
+  fi
+  echo "Selected option $option"
+  if [ "$1" = "0" ]
+  then
+    echo "Full VM Install"
+    full_vm
+    return 1
+  fi
+  if [ "$1" = "1" ]
+  then
+    echo "Python 3.7.9 Install"
+    py_install
+    return 1
+  fi
+  if [ "$1" = "2" ]
+  then
+    echo "Onos Install"
+    onos_install
+    return 1
+  fi
+  if [ "$1" = "3" ]
+  then
+    echo "Create Virtual Environment"
+    create_test_env
+    return 1
+  fi
+  if [ "$1" = "4" ]
+  then
+    echo "Remove Virtual Environment"
+    remove_test_env
+    return 1
+  fi
+  return 0
+}
+
+
 while :
 do
   prompt
@@ -80,6 +86,6 @@ do
   then
     echo Success!
   else
-    echo Something went wrong! Please try again. 
+    echo Something went wrong! Please try again.
   fi
 done
